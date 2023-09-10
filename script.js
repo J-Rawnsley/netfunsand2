348160253155c6f28008008d84348e2c7fa846cf
Joe Rawnsley <joe.rawnsley@gmail.com>
2023-09-10 16:15:16 +0100

    site updated to pull dog pictures from api call. the breed of dog can be set using an envirnment variable and is hidden from the user

--- script.js
+++ script.js
@@ -4,9 +4,18 @@ function setContent(text) {
 
 setContent("Hello from script.js")
 
+function displayImages(array) {
+    array.forEach(element => {
+        let thisImage = document.createElement("img")
+        thisImage.src = element
+        thisImage.classList.add("smaller-image")
+        document.body.appendChild(thisImage)
+    });
+}
+
 async function getData() {
-    // const url = "https://net-fun-sand-2.netlify.app/.netlify/functions/hello-world?name=from+netlify+function"
-    const url = "http://localhost:8888/.netlify/functions/hello-world?name=from+netlify+function+via+localhost"
+    const url = "https://net-fun-sand-2.netlify.app/.netlify/functions/hello-world?name=from+netlify+function"
+    // const url = "http://localhost:8888/.netlify/functions/hello-world?name=from+netlify+function+via+localhost"
 
     const response = await fetch(url)
     const json = await response.json()
@@ -20,4 +29,18 @@ async function getData() {
 }
 
 getData()
+
+async function getApiData() {
+    const url = "https://net-fun-sand-2.netlify.app/.netlify/functions/token-hider"
+    // const url = "http://localhost:8888/.netlify/functions/token-hider"
+
+    const response = await fetch(url)
+    const json = await response.json()
+
+    console.log(json.message)
+
+    displayImages(json.message)
+}
+
+getApiData()
 
